5fdde09e8686156e127126e409b645f608174c3d
NafuKrypto <nafisaalam131@gmail.com>
2024-11-04 17:54:23 +0600

    Filter in all products and saw how it worked with get_queryset

--- product/views.py
+++ product/views.py
@@ -11,3 +11,17 @@ from product.models import Product
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
+    # ct = 'products'
+    # def get_context_data(self, **kwargs):
+    #     ctx = super().get_context_data()
+    #     return ctx
+    def get_queryset(self):
+        queryset= super().get_queryset()
+        # filtered_data = c.filter(unit_price__lte=4.50)
+        # self.queryset = filtered_data
+        name = self.request.GET.get('name')
+
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+
+        return  queryset
