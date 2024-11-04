9a4992391cc0601183b326f1071f425310234f0e
NafuKrypto <nafisaalam131@gmail.com>
2024-11-04 15:20:33 +0600

    All Products view on ui

---
+++ product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+
+from product.views import ProductListView
+
+urlpatterns = [
+    path('lists',ProductListView.as_view(),name='product-list'),
+]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
+
+
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from product.models import Product
+
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    # ct = 'products'
