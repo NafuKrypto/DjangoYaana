527a2d4a4f1942d5e6c77d700a05fe40c2eae936
NafuKrypto <nafisaalam131@gmail.com>
2024-11-04 18:03:07 +0600

    un commenting unsual data

--- product/views.py
+++ product/views.py
@@ -1,6 +1,4 @@
 
-
-from django.shortcuts import render
 from django.views.generic import ListView
 
 from product.models import Product
@@ -11,14 +9,10 @@ from product.models import Product
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
-    # ct = 'products'
-    # def get_context_data(self, **kwargs):
-    #     ctx = super().get_context_data()
-    #     return ctx
+
     def get_queryset(self):
         queryset= super().get_queryset()
-        # filtered_data = c.filter(unit_price__lte=4.50)
-        # self.queryset = filtered_data
+
         name = self.request.GET.get('name')
 
         if name is not None:
